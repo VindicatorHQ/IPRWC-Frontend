6c0910aaaef618587b63e918b27ce39e13518ceb
Christiaan <minecraftgamer725@gmail.com>
2023-03-24 20:57:43 +0100

    Fixed styling issues

--- webshop/tailwind.config.js
+++ webshop/tailwind.config.js
@@ -4,6 +4,17 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    screens: {
+      'sm': '460px',
+
+      'md': '768px',
+
+      'lg': '1024px',
+
+      'xl': '1280px',
+
+      '2xl': '1536px',
+    },
     extend: {},
   },
   plugins: [],
