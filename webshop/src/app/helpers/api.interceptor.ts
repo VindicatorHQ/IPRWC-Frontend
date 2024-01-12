0e361bc512d0e9bf5b2f1abb14b4ab5ce5523a83
Christiaan <minecraftgamer725@gmail.com>
2024-01-12 23:16:04 +0100

    Changed request destination

--- webshop/src/app/helpers/api.interceptor.ts
+++ webshop/src/app/helpers/api.interceptor.ts
@@ -6,7 +6,7 @@ import {Observable} from 'rxjs';
 export class APIInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-    const apiReq = req.clone({url: `http://localhost:8081/v1/api${req.url}`});
+    const apiReq = req.clone({url: `http://159.223.211.149:8080/v1/api${req.url}`});
     return next.handle(apiReq);
   }
 }
